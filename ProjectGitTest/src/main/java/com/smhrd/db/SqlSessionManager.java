f153e79a284dad427a568bef2f3c5bda0fd5d8aa
smhrd <smhrd@61.80.80.189>
2023-02-20 15:37:57 +0900

    maven 으로 변경 오류는 모르겠음(선미)

---
+++ ProjectGitTest/src/main/java/com/smhrd/db/SqlSessionManager.java
@@ -0,0 +1,31 @@
+package com.smhrd.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.Resources;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionManager {
+
+
+	private static SqlSessionFactory SqlSessionFactory;
+	 
+	static {
+		try {
+			String resource = "com/smhrd/db/config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory =
+					new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+		
+		}
+	}
+	
+	public static SqlSessionFactory getSqlSession() {
+		return SqlSessionFactory;
+	}
+	
+}
