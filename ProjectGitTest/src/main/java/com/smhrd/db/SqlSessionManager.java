608000f2b4a0f07ce973c5e4e92d7946128e7e74
smhrd <smhrd@61.80.80.189>
2023-02-21 10:08:27 +0900

    에러확인

--- ProjectGitTest/src/main/java/com/smhrd/db/SqlSessionManager.java
+++ ProjectGitTest/src/main/java/com/smhrd/db/SqlSessionManager.java
@@ -3,8 +3,7 @@ package com.smhrd.db;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.annotation.Resources;
-
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
