a4a3cffbf222e74f9e7ff76240cca9fc9c5729c5
예지 <smhrd@211.48.213.178>
2023-03-02 17:29:10 +0900

    dao살ㄹ려어어

--- ProjectGitTest/src/main/java/com/smhrd/model/DAO_L.java
+++ ProjectGitTest/src/main/java/com/smhrd/model/DAO_L.java
@@ -1,10 +1,23 @@
 package com.smhrd.model;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.db.SqlSessionManager;
+
 public class DAO_L {
 // 이철민
 //		┌───────────────────────┐
 // 		│	쳐다보지 마시오 			│
 //		│				-주인백	│
 //		└───────────────────────┘
-	
+	private SqlSessionFactory sqlSesstionFAcFactory = SqlSessionManager.getSqlSession();
+	//회원가입
+		public int targetadd(targetVO vo) {
+			SqlSession session = sqlSesstionFAcFactory.openSession(true);
+			int cnt = session.insert("targetadd", vo);
+			session.close();
+			return cnt;
+			
+		}
 }
